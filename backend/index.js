7f04a5d70a107be399508bf59c230a37190539f1
Anwishta <anwishtaghosh@gmail.com>
2024-12-25 16:56:03 +0530

    index.js updated

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
