9aa56f2f89fd41e5ec898ca493ad9ebb20e0cc1a
Anwishta <anwishtaghosh@gmail.com>
2025-01-01 12:13:13 +0530

    .....

--- backend/index.js
+++ backend/index.js
@@ -33,6 +33,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.get("/favicon.ico", (req, res) => {
+  res.send("API is running...");
+})
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
 // app.use("/api/products", productRoutes);
