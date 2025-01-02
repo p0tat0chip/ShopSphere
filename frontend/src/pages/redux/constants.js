5b99846f34614b3dd8be581489b9aa8b78436313
p0tat0chip <mr.mayankgame@gmail.com>
2025-01-02 23:22:48 +0530

    Fix #15

--- frontend/src/pages/redux/constants.js
+++ frontend/src/pages/redux/constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL = process.env.NODE_ENV === 'production'
-  ? 'https://shop-sphere-liard.vercel.app/' 
+  ? 'https://shop-sphere-2n6k.vercel.app/'
   : '';
 export const USERS_URL = '/api/users'
 export const CATEGORY_URL = '/api/category'
