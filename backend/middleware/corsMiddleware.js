827864577c855bc1771926f4654065439b0959bf
p0tat0chip <166798988+p0tat0chip@users.noreply.github.com>
2025-01-02 13:56:55 +0000

    test

--- backend/middleware/corsMiddleware.js
+++ backend/middleware/corsMiddleware.js
@@ -1,9 +1,9 @@
 import cors from 'cors';
 
 const corsOptions = {
-    origin: 'https://shop-sphere-2n6k.vercel.app', 
+    origin: 'https://shop-sphere-auu2-git-testcors-mynk8s-projects.vercel.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true, 
+    credentials: true,
 };
 
 const corsMiddleware = cors(corsOptions);
