a50fec1ce26ceb7b1faf154ff5a09e09111b2e4f
p0tat0chip <166798988+p0tat0chip@users.noreply.github.com>
2025-01-02 14:30:18 +0000

    test

--- backend/index.js
+++ backend/index.js
@@ -12,9 +12,9 @@ import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
-import corsMiddleware from "./middleware/corsMiddleware.js";
+// import corsMiddleware from "./middleware/corsMiddleware.js";
 
-app.use(corsMiddleware);
+// app.use(corsMiddleware);
 dotenv.config();
 const port = process.env.PORT || 5000;
 
@@ -35,6 +35,10 @@ app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
 app.get("/", (req, res) => {
   res.send("API is running!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
 export default app;
