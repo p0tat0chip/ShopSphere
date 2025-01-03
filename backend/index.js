535239def56e3bbadebe319894166e648a7bd9cc
Anwishta <anwishtaghosh@gmail.com>
2025-01-03 22:08:26 +0530

    products

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(cors(corsOptions));
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
-import productRoutes from "./routes/productRoutes.js";
+// import productRoutes from "./routes/productRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
@@ -38,7 +38,7 @@ app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
-app.use("/api/products", productRoutes);
+// app.use("/api/products", productRoutes);
 app.use("/api/upload", uploadRoutes);
 
 const __dirname = path.resolve();
