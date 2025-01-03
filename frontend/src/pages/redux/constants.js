090d4ecbb24c75bc69e40b28665b545b75f5cbab
Anwishta <anwishtaghosh@gmail.com>
2025-01-03 20:49:37 +0530

    constants

--- frontend/src/pages/redux/constants.js
+++ frontend/src/pages/redux/constants.js
@@ -1,8 +1,8 @@
 export const BASE_URL = process.env.NODE_ENV === 'production'
-  ? 'https://shop-sphere-liard.vercel.app/' 
-  : '';
-export const USERS_URL = '/api/users'
-export const CATEGORY_URL = '/api/category'
-export const PRODUCT_URL = '/api/products'
-export const UPLOAD_URL = '/api/upload'
-// export const REACT_APP_API_URL= 'https://shopsphere-backend-9xia.onrender.com'
+  ? 'https://shop-sphere-liard.vercel.app' 
+  : 'http://localhost:5000';
+
+export const USERS_URL = `${BASE_URL}/api/users`;
+export const CATEGORY_URL = `${BASE_URL}/api/category`;
+export const PRODUCT_URL = `${BASE_URL}/api/products`;
+export const UPLOAD_URL = `${BASE_URL}/api/upload`;
