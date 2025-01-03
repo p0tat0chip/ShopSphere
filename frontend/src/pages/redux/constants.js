840164ebaae239767ebb5e54507eb0f6297a5de2
Anwishta <anwishtaghosh@gmail.com>
2025-01-03 20:57:51 +0530

    constants.js

--- frontend/src/pages/redux/constants.js
+++ frontend/src/pages/redux/constants.js
@@ -1,6 +1,6 @@
-export const BASE_URL = process.env.NODE_ENV === 'production'
-  ? 'https://shop-sphere-liard.vercel.app' 
-  : 'http://localhost:5000';
+
+
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000'; // Fallback for local development
 
 export const USERS_URL = `${BASE_URL}/api/users`;
 export const CATEGORY_URL = `${BASE_URL}/api/category`;
